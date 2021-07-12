0195407a429fb348111bc801cdbc6b02d194c721
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-12 19:23:16 -0400

    Implemented Listing Page

--- app/src/main/java/com/example/booksmart/ui/listings/ListingsFragment.java
+++ app/src/main/java/com/example/booksmart/ui/listings/ListingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.booksmart.ui.listings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,35 +12,68 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.booksmart.R;
+import com.example.booksmart.adapters.ListingAdapter;
 import com.example.booksmart.databinding.FragmentListingsBinding;
+import com.example.booksmart.models.Listing;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListingsFragment extends Fragment {
 
-    private ListingsViewModel listingsViewModel;
-    private FragmentListingsBinding binding;
+    public static final String TAG = "ListingsFragment";
+    public static final int GRID_SPAN = 2;
+    public static final int LISTING_LIMIT = 20;
+    public static final String DESCENDING_ORDER_KEY = "createdAt";
+    public static final String QUERY_ERROR = "Error getting listings";
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        listingsViewModel =
-                new ViewModelProvider(this).get(ListingsViewModel.class);
+    List<Listing> listings;
+    RecyclerView rvListings;
+    ListingAdapter adapter;
+    GridLayoutManager gridLayoutManager;
 
-        binding = FragmentListingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        final TextView textView = binding.textHome;
-        listingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_listings, container, false);
+
+        listings = new ArrayList<>();
+        adapter = new ListingAdapter(getContext(), listings);
+        gridLayoutManager = new GridLayoutManager(getContext(), GRID_SPAN);
+        rvListings = view.findViewById(R.id.rvListing);
+
+        rvListings.setLayoutManager(gridLayoutManager);
+        rvListings.setAdapter(adapter);
+
+        queryListings();
+
+        return view;
+    }
+
+    private void queryListings() {
+        ParseQuery<Listing> query = ParseQuery.getQuery(Listing.class);
+        query.include(Listing.KEY_USER);
+        query.setLimit(LISTING_LIMIT);
+        query.addDescendingOrder(DESCENDING_ORDER_KEY);
+        query.findInBackground(new FindCallback<Listing>() {
+
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void done(List<Listing> allListings, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, QUERY_ERROR, e);
+                    return;
+                }
+                
+                listings.addAll(allListings);
+                adapter.notifyDataSetChanged();
             }
         });
-        return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
