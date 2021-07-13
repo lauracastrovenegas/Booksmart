66651a437468e4ff99bb17774f146e533f5665dd
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-13 18:22:26 -0400

    Set up basic skeleton for listing form fragment

--- app/src/main/java/com/example/booksmart/ui/listings/ListingFormFragment.java
+++ app/src/main/java/com/example/booksmart/ui/listings/ListingFormFragment.java
@@ -3,64 +3,93 @@ package com.example.booksmart.ui.listings;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.booksmart.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListingFormFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListingFormFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText etTitle;
+    EditText etDescription;
+    EditText etCourse;
+    EditText etPrice;
+    Button btnSelectPhoto;
+    Button btnCapturePhoto;
+    Button btnPost;
+    ImageView ivCloseForm;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListingFormFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListingFormFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListingFormFragment newInstance(String param1, String param2) {
-        ListingFormFragment fragment = new ListingFormFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public ListingFormFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listing_form, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_listing_form, container, false);
+
+        etTitle = view.findViewById(R.id.etListingTitle);
+        etDescription = view.findViewById(R.id.etListingDescription);
+        etCourse = view.findViewById(R.id.etListingCourse);
+        etPrice = view.findViewById(R.id.etListingPrice);
+        btnSelectPhoto = view.findViewById(R.id.btnListingSelect);
+        btnCapturePhoto = view.findViewById(R.id.btnListingCapture);
+        btnPost = view.findViewById(R.id.btnListingPost);
+        ivCloseForm = view.findViewById(R.id.ivPostClose);
+
+        btnCapturePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLaunchCamera();
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPost();
+            }
+        });
+
+        ivCloseForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goListingTimeline();
+            }
+        });
+
+        return view;
+    }
+
+    private void onPost() {
+        //TODO
+    }
+
+    private void onLaunchCamera() {
+        //TODO
+    }
+
+    private void goListingTimeline(){
+        Fragment fragment = new ListingsFragment();
+        replaceFragment(fragment);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment_activity_main, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
