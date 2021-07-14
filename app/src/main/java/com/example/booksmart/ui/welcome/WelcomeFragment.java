e974259fd0c088f6560b13273d2fe99128093b64
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-14 19:42:17 -0400

    Added onClickListeners for signup and login buttons to navigate to those fragments

--- app/src/main/java/com/example/booksmart/ui/welcome/WelcomeFragment.java
+++ app/src/main/java/com/example/booksmart/ui/welcome/WelcomeFragment.java
@@ -3,14 +3,22 @@ package com.example.booksmart.ui.welcome;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.booksmart.MainActivity;
 import com.example.booksmart.R;
+import com.example.booksmart.WelcomeActivity;
+import com.example.booksmart.ui.listings.ListingsFragment;
 
 public class WelcomeFragment extends Fragment {
+
+    Button btnLogin;
+    Button btnSignup;
 
     public WelcomeFragment() {}
 
@@ -19,7 +27,33 @@ public class WelcomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
 
+        btnLogin = view.findViewById(R.id.btnLogin);
+        btnSignup = view.findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               goLoginFragment();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignupFragment();
+            }
+        });
 
         return view;
     }
+
+    private void goLoginFragment(){
+        ((WelcomeActivity) getActivity()).replaceFragment(new LoginFragment());
+    }
+
+    private void goSignupFragment(){
+        ((WelcomeActivity) getActivity()).replaceFragment(new SignupFragment());
+    }
+
+
 }
