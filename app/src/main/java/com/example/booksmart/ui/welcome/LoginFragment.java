ba59b8ba4c31886cfa4f5f0f6f6d4942c0f5c2b6
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-14 19:41:42 -0400

    Created login fragment and layout

---
+++ app/src/main/java/com/example/booksmart/ui/welcome/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.example.booksmart.ui.welcome;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.booksmart.R;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return view;
+    }
+}
