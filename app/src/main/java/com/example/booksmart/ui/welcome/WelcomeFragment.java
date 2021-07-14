20302fd42972ba8cc40a0db47fc3c01abbd11b7d
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-14 17:02:43 -0400

    Created and set layout for welcome fragment

---
+++ app/src/main/java/com/example/booksmart/ui/welcome/WelcomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.booksmart.ui.welcome;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.booksmart.R;
+
+public class WelcomeFragment extends Fragment {
+
+    public WelcomeFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+
+        return view;
+    }
+}
