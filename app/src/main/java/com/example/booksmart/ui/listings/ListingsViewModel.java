c616025dc42ba29ff7853dd0b79ecb287ccceae8
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-21 20:27:00 -0400

    Implemented ViewModel for Listings Fragment
    - ViewModel now keeps track of list of items and calls the client fetch and queries directly and updates the list

--- app/src/main/java/com/example/booksmart/ui/listings/ListingsViewModel.java
+++ app/src/main/java/com/example/booksmart/ui/listings/ListingsViewModel.java
@@ -1,20 +1,88 @@
 package com.example.booksmart.ui.listings;
 
+import android.app.Application;
+import android.util.Log;
+import android.view.View;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.booksmart.Client;
+import com.example.booksmart.MainActivity;
+import com.example.booksmart.adapters.ListingAdapter;
+import com.example.booksmart.models.Book;
 import com.example.booksmart.models.Item;
+import com.example.booksmart.models.Listing;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
-public class ListingsViewModel extends ViewModel {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private MutableLiveData<Item> selected = new MutableLiveData<Item>();
+import java.util.ArrayList;
+import java.util.List;
 
-    public void select(Item item){
-        selected.setValue(item);
+public class ListingsViewModel extends AndroidViewModel {
+
+    public static final String TAG = "ListingsFragmentViewModel";
+
+    MutableLiveData<List<Item>> items;
+    List<Item> itemArrayList;
+    Client client;
+    int skip;
+    long startIndex;
+
+    public ListingsViewModel(Application application) {
+        super(application);
+
+        itemArrayList = new ArrayList<>();
+        items = new MutableLiveData<>();
+        skip = 0;
+        startIndex = 0;
+
+        setClient(application);
+
+        client.onInitialLoad();
     }
 
-    public LiveData<Item> getSelected() {
-        return selected;
+    private void setClient(Application application) {
+        client = new Client(application.getBaseContext()) {
+            @Override
+            public void onDone(List<Item> fetchedItems) {
+                itemArrayList.addAll(fetchedItems);
+                items.setValue(itemArrayList);
+
+                skip = client.getCurrentSkip();
+                startIndex = client.getCurrentStart();
+            }
+        };
+    }
+
+    public LiveData<List<Item>> getItems() {
+        return items;
+    }
+
+    public List<Item> getItemArrayList(){
+        return itemArrayList;
+    }
+
+    public void fetchMoreItems(){
+        client.fetchItems(skip, startIndex);
+    }
+
+    public void resetList(){
+        itemArrayList.clear();
+        skip = 0;
+        startIndex = 0;
+        fetchMoreItems();
     }
 }
