93b8fcd00422a946e7fa644e48f099eca088f88f
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-22 18:26:01 -0400

    Added previousFragment variable to  ListingDetailFragment hold previous fragment to return to on back

--- app/src/main/java/com/example/booksmart/ui/listings/ListingDetailViewModel.java
+++ app/src/main/java/com/example/booksmart/ui/listings/ListingDetailViewModel.java
@@ -1,14 +1,17 @@
 package com.example.booksmart.ui.listings;
 
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.booksmart.models.Item;
+import com.example.booksmart.ui.profile.ProfileFragment;
 
 public class ListingDetailViewModel extends ViewModel {
 
     private MutableLiveData<Item> selected = new MutableLiveData<Item>();
+    private MutableLiveData<Fragment> previousFragment = new MutableLiveData<>();
 
     public void select(Item item){
         selected.setValue(item);
@@ -16,5 +19,13 @@ public class ListingDetailViewModel extends ViewModel {
 
     public LiveData<Item> getSelected() {
         return selected;
+    }
+
+    public void setPreviousFragment(Fragment fragment) {
+        previousFragment.setValue(fragment);
+    }
+
+    public LiveData<Fragment> getPreviousFragment(){
+        return previousFragment;
     }
 }
