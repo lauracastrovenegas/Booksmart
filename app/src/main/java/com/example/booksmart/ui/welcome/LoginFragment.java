93610b37e20ab965e712bfde720fbdbacddb05f0
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-26 12:36:47 -0400

    Updated login fragment to use ParseClient to handle user login

--- app/src/main/java/com/example/booksmart/ui/welcome/LoginFragment.java
+++ app/src/main/java/com/example/booksmart/ui/welcome/LoginFragment.java
@@ -16,10 +16,17 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.booksmart.R;
+import com.example.booksmart.helpers.ParseClient;
+import com.example.booksmart.models.Item;
+import com.example.booksmart.models.Listing;
 import com.example.booksmart.ui.WelcomeActivity;
 import com.parse.LogInCallback;
+import com.parse.ParseClassName;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class LoginFragment extends Fragment {
 
@@ -35,6 +42,7 @@ public class LoginFragment extends Fragment {
     EditText etPassword;
     Button btnSignin;
     ProgressBar pb;
+    ParseClient parseClient;
 
     public LoginFragment() {}
 
@@ -49,6 +57,8 @@ public class LoginFragment extends Fragment {
         btnSignin = view.findViewById(R.id.btnSignIn);
         pb = view.findViewById(R.id.pbLoadingLogin);
 
+        setParseClient();
+
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,14 +72,14 @@ public class LoginFragment extends Fragment {
                 pb.setVisibility(View.VISIBLE);
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
-                loginUser(username, password);
+                onLogin(username, password);
             }
         });
 
         return view;
     }
 
-    private void loginUser(String username, String password) {
+    private void onLogin(String username, String password) {
         if (username.isEmpty() || username == null){
             pb.setVisibility(View.INVISIBLE);
             Toast.makeText(getContext(), NO_USERNAME_MSG, Toast.LENGTH_SHORT).show();
@@ -81,41 +91,37 @@ public class LoginFragment extends Fragment {
             return;
         }
 
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
+        parseClient.loginUser(username, password);
+    }
+
+    private void setParseClient() {
+        parseClient = new ParseClient(getContext()) {
             @Override
-            public void done(ParseUser user, ParseException e) {
+            public void onUserLoggedIn() {
                 pb.setVisibility(View.INVISIBLE);
-                if (e != null) {
-                    switch (e.getCode()) {
-                        case ParseException.USERNAME_MISSING:
-                            Toast.makeText(getContext(), NO_USERNAME_MSG, Toast.LENGTH_SHORT).show();
-                            break;
-                        case ParseException.PASSWORD_MISSING:
-                            Toast.makeText(getContext(), NO_PASSWORD_MSG, Toast.LENGTH_SHORT).show();
-                            break;
-                        case ParseException.OBJECT_NOT_FOUND:
-                            Toast.makeText(getContext(), INVALID_LOGIN_MSG, Toast.LENGTH_SHORT).show();
-                            break;
-                        default:
-                            Toast.makeText(getContext(), LOGIN_ISSUE_MSG, Toast.LENGTH_SHORT).show();
-                            break;
-                    }
-
-                    Log.d(TAG, LOGIN_ISSUE_MSG + e.getMessage(), e);
-                    return;
-                }
-
                 ((WelcomeActivity) getActivity()).goMainActivity();
                 Toast.makeText(getContext(), SUCCESS_MSG, Toast.LENGTH_SHORT).show();
             }
-        });
+
+            @Override
+            public void onUserFetched(ParseUser user) {}
+
+            @Override
+            public void onListingSaved(Listing listing) {}
+
+            @Override
+            public void onQueryListingsDone(List<Item> items) {}
+
+            @Override
+            public void onParseImageSaved(ParseFile image) {}
+        };
     }
 
-    public void goWelcomeFragment(){
+    private void goWelcomeFragment(){
         replaceFragment(new WelcomeFragment());
     }
 
-    public void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment) {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out);
         transaction.replace(R.id.placeholder_activity_welcome, fragment);
