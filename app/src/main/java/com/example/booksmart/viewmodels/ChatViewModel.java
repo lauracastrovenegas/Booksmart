ef0ca8c320696fcb7940867ef7a05a7365b5ee1d
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-27 13:22:29 -0400

    Implemented ChatViewModel

--- app/src/main/java/com/example/booksmart/viewmodels/ChatViewModel.java
+++ app/src/main/java/com/example/booksmart/viewmodels/ChatViewModel.java
@@ -1,7 +1,38 @@
 package com.example.booksmart.viewmodels;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ChatViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.example.booksmart.models.Conversation;
+import com.example.booksmart.models.Listing;
+import com.example.booksmart.models.Message;
+
+import java.util.List;
+
+public class ChatViewModel extends AndroidViewModel {
+
+    MutableLiveData<Conversation> conversation;
+    MutableLiveData<List<Message>> messages;
+
+    public ChatViewModel(Application application){
+        super(application);
+
+        conversation = new MutableLiveData<>();
+        messages = new MutableLiveData<>();
+    }
+
+    public void select(Conversation conversation) {
+        this.conversation.setValue(conversation);
+    }
+
+    public MutableLiveData<Conversation> getSelected() {
+        return conversation;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages.setValue(messages);
+    }
 }
