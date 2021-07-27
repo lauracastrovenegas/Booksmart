8671c65b24efbc54bdffdcaf125eb5d0504c796f
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-27 13:23:15 -0400

    Added on item click method in onViewCreated in Conversation Fragment class to navigate to chat view and pass Conversation to view mdoel

--- app/src/main/java/com/example/booksmart/ui/chat/ConversationsFragment.java
+++ app/src/main/java/com/example/booksmart/ui/chat/ConversationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.booksmart.ui.chat;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -17,9 +18,14 @@ import android.view.ViewGroup;
 
 import com.example.booksmart.R;
 import com.example.booksmart.adapters.ChatPreviewAdapter;
+import com.example.booksmart.helpers.ItemClickSupport;
 import com.example.booksmart.models.Conversation;
+import com.example.booksmart.models.Item;
+import com.example.booksmart.ui.listings.ListingsFragment;
 import com.example.booksmart.viewmodels.ChatViewModel;
 import com.example.booksmart.viewmodels.ConversationsViewModel;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -50,6 +56,23 @@ public class ConversationsFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Click listener for items in the recycler view
+        ItemClickSupport.addTo(rvConversations).setOnItemClickListener(
+                new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        Conversation conversation = conversationsViewModel.getConversation(position);
+                        chatViewModel.select(conversation);
+                        goChatView();
+                    }
+                }
+        );
+    }
+
     private void setViewModels() {
         chatViewModel = new ViewModelProvider((requireActivity())).get(ChatViewModel.class);
         conversationsViewModel = new ViewModelProvider(requireActivity()).get(ConversationsViewModel.class);
@@ -61,6 +84,19 @@ public class ConversationsFragment extends Fragment {
                 rvConversations.setAdapter(adapter);
             }
         });
+    }
+
+    private void goChatView() {
+        Fragment fragment = new ChatFragment();
+        replaceFragment(fragment);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out_left);
+        transaction.replace(R.id.nav_host_fragment_activity_main, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
