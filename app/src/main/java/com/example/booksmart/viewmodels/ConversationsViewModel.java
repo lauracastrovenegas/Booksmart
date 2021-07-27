0a65d754821be288c56446a5c42103a9745e5dea
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-27 12:59:33 -0400

    Implemented View Model for Conversations Fragment

--- app/src/main/java/com/example/booksmart/viewmodels/ConversationsViewModel.java
+++ app/src/main/java/com/example/booksmart/viewmodels/ConversationsViewModel.java
@@ -1,7 +1,44 @@
 package com.example.booksmart.viewmodels;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class ConversationsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.booksmart.models.Conversation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversationsViewModel extends AndroidViewModel {
+
+    public static final String TAG = "conversationViewModel";
+
+    MutableLiveData<List<Conversation>> conversations;
+    List<Conversation> conversationList;
+
+    public ConversationsViewModel(Application application){
+        super(application);
+
+        conversationList = new ArrayList<>();
+        conversations = new MutableLiveData<>();
+
+        setRepository();
+    }
+
+    private void setRepository() {
+        // TODO: Implement repository
+    }
+
+    public MutableLiveData<List<Conversation>> getConversations() {
+        return conversations;
+    }
+
+    public List<Conversation> getConversationList() {
+        return conversationList;
+    }
+
+    public Conversation getConversation(int position){
+        return conversationList.get(position);
+    }
 }
