daa6c6df0e0d9b1beecf73d902959f6cc8513261
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-27 16:30:29 -0400

    Added new get time formatted method for message time stamp in minutes

--- app/src/main/java/com/example/booksmart/models/Message.java
+++ app/src/main/java/com/example/booksmart/models/Message.java
@@ -14,6 +14,7 @@ public class Message extends ParseObject {
     public static final String USER_KEY = "user";
     public static final String BODY_KEY = "body";
     private static final String DATE_FORMAT = "MMMM dd, yyyy";
+    private static final String TIME_FORMAT = "hh:mm";
 
     public ParseUser getUser() {
         return getParseUser(USER_KEY);
@@ -35,6 +36,10 @@ public class Message extends ParseObject {
         return calculateTimeAgo(getCreatedAt());
     }
 
+    public String getCreatedAtTime(){
+        return calculateTimeAgoMinutes(getCreatedAt());
+    }
+
     public static String calculateTimeAgo(Date createdAt) {
 
         int SECOND_MILLIS = 1000;
@@ -54,12 +59,36 @@ public class Message extends ParseObject {
                 return "a minute ago";
             } else if (diff < 50 * MINUTE_MILLIS) {
                 return diff / MINUTE_MILLIS + " m";
-            } else if (diff < 90 * MINUTE_MILLIS) {
-                return "an hour ago";
             } else if (diff < 24 * HOUR_MILLIS) {
                 return diff / HOUR_MILLIS + " h";
             } else if (diff < 48 * HOUR_MILLIS) {
                 return "yesterday";
+            } else {
+                return DateFormat.format(DATE_FORMAT, createdAt).toString();
+            }
+        } catch (Exception e) {
+            Log.i("Error:", "getRelativeTimeAgo failed", e);
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static String calculateTimeAgoMinutes(Date createdAt) {
+
+        int SECOND_MILLIS = 1000;
+        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        try {
+            createdAt.getTime();
+            long time = createdAt.getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < 24 * HOUR_MILLIS) {
+                return DateFormat.format(TIME_FORMAT, createdAt).toString();
             } else {
                 return DateFormat.format(DATE_FORMAT, createdAt).toString();
             }
