8b1ab6d06086395701382f1f3ea2c87278dff94d
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-28 19:12:38 -0400

    Implemented methods in ParseMessageClient to save a message, get all messages for a conversation, and get the last message of a conversation

--- app/src/main/java/com/example/booksmart/helpers/ParseMessageClient.java
+++ app/src/main/java/com/example/booksmart/helpers/ParseMessageClient.java
@@ -3,6 +3,8 @@ package com.example.booksmart.helpers;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.booksmart.models.Conversation;
 import com.example.booksmart.models.Listing;
 import com.example.booksmart.models.Message;
@@ -18,14 +20,16 @@ import com.parse.SaveCallback;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class ParseMessageClient {
+public class ParseMessageClient {
 
     public static final String TAG = "ParseMessageClient";
     public static final String DESCENDING_ORDER_KEY = "createdAt";
     public static final String MESSAGES_KEY = "messages";
     public static final String USERS_KEY = "users";
-    public static final String QUERY_ERROR = "Unable to fetch all conversations";
+    public static final String CONVO_QUERY_ERROR = "Unable to fetch all conversations";
     public static final String LISTING_KEY = "listing";
+    private static final String USER_KEY = "user";
+    private static final String CONVO_KEY = "conversation";
 
     Context context;
 
@@ -45,7 +49,7 @@ public abstract class ParseMessageClient {
             @Override
             public void done(List<Conversation> conversations, ParseException e) {
                 if (e != null){
-                    Log.e(TAG, QUERY_ERROR, e);
+                    Log.e(TAG, CONVO_QUERY_ERROR, e);
                     return;
                 }
 
@@ -63,13 +67,74 @@ public abstract class ParseMessageClient {
                     Log.e(TAG, e.getMessage(), e);
                 }
 
-                Log.i(TAG, conversation.toString());
                 onNewConversationSaved(conversation);
             }
         });
     }
 
-    protected abstract void onNewConversationSaved(Conversation conversation);
+    public void saveMessage(Message message){
+        message.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, e.getMessage(), e);
+                }
 
-    public abstract void onAllConversationsFetched(List<Conversation> conversation);
+                onMessageSaved(message);
+            }
+        });
+    }
+
+    public void getMessages(Conversation conversation) {
+        ParseQuery query = ParseQuery.getQuery(Message.class);
+        query.include(USER_KEY);
+        query.include(CONVO_KEY);
+        query.whereEqualTo(CONVO_KEY, conversation);
+        query.addDescendingOrder(DESCENDING_ORDER_KEY);
+        query.findInBackground(new FindCallback<Message>() {
+            @Override
+            public void done(List<Message> messages, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, e.getMessage(), e);
+                    return;
+                }
+
+                onAllMessagesFetched(messages);
+            }
+        });
+    }
+
+    public void getLastMessage(Conversation conversation) {
+        ParseQuery query = ParseQuery.getQuery(Message.class);
+        query.whereEqualTo(CONVO_KEY, conversation);
+        query.addDescendingOrder(DESCENDING_ORDER_KEY);
+
+        query.getFirstInBackground(new GetCallback<Message>() {
+            @Override
+            public void done(Message message, ParseException e) {
+                if (e != null){
+                    if (e.getCode() == ParseException.OBJECT_NOT_FOUND){
+                        onMessageFetched(null);
+                    } else {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
+                    return;
+                }
+
+                onMessageFetched(message);
+            }
+        });
+    }
+
+    protected void onMessageFetched(Message message){};
+
+    protected void onAllMessagesFetched(List<Message> messages){};
+
+    protected void onMessageSaved(Message message){};
+
+    protected void onNewConversationSaved(Conversation conversation){};
+
+    protected void onAllConversationsFetched(List<Conversation> conversation){};
+
+
 }
