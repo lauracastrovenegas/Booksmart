6b50af2be8f85a44361cf109664e5cdfea1b4176
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-28 19:13:51 -0400

    Implemented a repository for chat/updated chat view model to utilize the chat repository

---
+++ app/src/main/java/com/example/booksmart/ChatRepository.java
@@ -0,0 +1,62 @@
+package com.example.booksmart;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.booksmart.helpers.ParseMessageClient;
+import com.example.booksmart.models.Conversation;
+import com.example.booksmart.models.Message;
+
+import java.util.List;
+
+public class ChatRepository {
+
+    MutableLiveData<Conversation> conversation;
+    MutableLiveData<List<Message>> messages;
+    ParseMessageClient parseClient;
+    Context context;
+
+    public ChatRepository(Application application){
+        conversation = new MutableLiveData<>();
+        messages = new MutableLiveData<>();
+        context = application.getBaseContext();
+
+        setClient();
+    }
+
+    private void setClient() {
+        parseClient = new ParseMessageClient(context) {
+
+            @Override
+            protected void onAllMessagesFetched(List<Message> allMessages) {
+                messages.setValue(allMessages);
+            }
+
+            @Override
+            protected void onMessageSaved(Message message) {
+                parseClient.getMessages(conversation.getValue());
+            }
+
+        };
+    }
+
+    public MutableLiveData<List<Message>> getMessages() {
+        return messages;
+    }
+
+    public void fetchMessages(){
+        if (conversation.getValue() != null){
+            parseClient.getMessages(conversation.getValue());
+        }
+    }
+
+    public MutableLiveData<Conversation> getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation.setValue(conversation);
+    }
+}
--- app/src/main/java/com/example/booksmart/viewmodels/ChatViewModel.java
+++ app/src/main/java/com/example/booksmart/viewmodels/ChatViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.booksmart.ChatRepository;
 import com.example.booksmart.models.Conversation;
 import com.example.booksmart.models.Listing;
 import com.example.booksmart.models.Message;
@@ -16,16 +17,20 @@ public class ChatViewModel extends AndroidViewModel {
 
     MutableLiveData<Conversation> conversation;
     MutableLiveData<List<Message>> messages;
+    ChatRepository chatRepository;
 
     public ChatViewModel(Application application){
         super(application);
 
         conversation = new MutableLiveData<>();
-        messages = new MutableLiveData<>();
+        chatRepository = new ChatRepository(application);
+        messages = chatRepository.getMessages();
     }
 
     public void select(Conversation conversation) {
-        this.conversation.setValue(conversation);
+        chatRepository.setConversation(conversation);
+        this.conversation = chatRepository.getConversation();
+        chatRepository.fetchMessages();
     }
 
     public MutableLiveData<Conversation> getSelected() {
