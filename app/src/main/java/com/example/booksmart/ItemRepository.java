17b0db8f4d43306f8b2d05330e3507b75a1de1e6
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-29 22:53:08 -0400

    Removed book fetching method from repository, updated methods accordingly

--- app/src/main/java/com/example/booksmart/ItemRepository.java
+++ app/src/main/java/com/example/booksmart/ItemRepository.java
@@ -5,12 +5,15 @@ import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.booksmart.helpers.GoogleBooksClient;
 import com.example.booksmart.helpers.ParseClient;
 import com.example.booksmart.models.Book;
 import com.example.booksmart.models.Item;
@@ -31,7 +34,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class ItemRepository {
+public class ItemRepository {
 
     public static final String TAG = "Listings Client";
     public static final int LISTING_LIMIT = 15;
@@ -41,71 +44,60 @@ public abstract class ItemRepository {
     public static final String ITEMS_KEY = "items";
 
     Context context;
-    RequestQueue queue;
+    GoogleBooksClient googleClient;
     ParseClient parseClient;
-    List<Item> items;
-    ParseUser currentUser;
-    int listingSkip;
+    MutableLiveData<List<Item>> items;
+    List<Item> itemList;
     int skip;
     long startIndex;
-    String userSchool;
     String title;
     String description;
     String price;
     String course;
-    Boolean isInitialFetch;
 
     public ItemRepository(Context context){
         this.context = context;
         setParseClient();
-        queue = Volley.newRequestQueue(context);
-        items = new ArrayList<>();
+        setGoogleClient();
+        itemList = new ArrayList<>();
+        items = new MutableLiveData<>();
         skip = 0;
         startIndex = 0;
-        isInitialFetch = false;
+
+        fetchItems(0, 0, "");
+    }
+
+    private void setGoogleClient() {
+        googleClient = new GoogleBooksClient(context){
+            @Override
+            public void onBooksFetched(List<Book> newBooks) {
+                Log.i(TAG, "onBooksFetched()");
+                startIndex = startIndex + newBooks.size();
+                //Log.i(TAG, newBooks.get(0).getTitle());
+                itemList.addAll(newBooks);
+                items.setValue(itemList);
+                //Log.i(TAG, ((Book) items.getValue().get(0)).getTitle());
+                Log.i(TAG, String.valueOf(itemList.size()));
+                onAllItemsFetched(itemList);
+            }
+        };
     }
 
     private void setParseClient() {
         parseClient = new ParseClient(context) {
             @Override
-            public void onUserLoggedIn() {}
-
-            @Override
-            public void onUserFetched(ParseUser user) {
-                Log.i(TAG, "onUserFetched()");
-                currentUser = user;
-                userSchool = user.getString(KEY_SCHOOL);
-                if (isInitialFetch){
-                    fetchItems(0,0);
-                }
-            }
-
-            @Override
-            public void onQueryUserListingsDone(List<Listing> allListings, ParseException e) {
-                listingSkip = listingSkip + allListings.size();
-                List<Item> newList = new ArrayList<>();
-                newList.addAll(allListings);
-                onAllItemsFetched(newList);
-            }
-
-            @Override
-            public void onQueryListingsDone(List<Listing> allListings, ParseException e) {
+            public void onQueryListingsDone(List<Listing> allListings, String queryString, ParseException e) {
                 Log.i(TAG, "onQueryListingsDone()");
                 if (e != null){
                     Log.e(TAG, QUERY_ERROR, e);
-                    fetchBooks(DEFAULT_QUERY, startIndex);
+                    googleClient.fetchBooks(queryString, startIndex);
                     return;
                 }
 
-                if (skip == 0){
-                    items.clear();
-                    startIndex = 0;
-                }
-
-                items.addAll(allListings);
+                itemList.addAll(allListings);
                 skip = skip + allListings.size();
 
-                fetchBooks(DEFAULT_QUERY, startIndex);
+                googleClient.fetchBooks(queryString, startIndex);
             }
 
             @Override
@@ -115,53 +107,28 @@ public abstract class ItemRepository {
 
             @Override
             public void onListingSaved(Listing listing) {
-                listingSaved(listing);
+                fetchItems(0, 0, "");
             }
         };
     }
 
-    public void onInitialLoad(){
-        Log.i(TAG, "onInitialLoad()");
-        isInitialFetch = true;
-        parseClient.getCurrentUser();
+    public MutableLiveData<List<Item>> getItems() {
+        return items;
     }
 
-    public void fetchItems(int skipValue, long startIndexValue){
+    public void fetchItems(int skipValue, long startIndexValue, String query){
         Log.i(TAG, "fetchItems()");
-        items.clear();
+        skip = skipValue;
         startIndex = startIndexValue;
-        parseClient.queryListings(skipValue);
+        if (skip == 0 && startIndexValue == 0){
+            itemList.clear();
+        }
+        parseClient.queryListings(skipValue, query);
     }
 
-    public void queryUserListings(int skipValue, ParseUser user) {
-        parseClient.queryUserListings(skipValue, user);
-    }
-
-    private void fetchBooks(String queryString, long start){
-        Log.i(TAG, "fetchBooks()");
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, GOOGLE_BOOKS_URL + queryString + "&startIndex=" + String.valueOf(start), null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            List<Book> newBooks = Book.fromJsonArray(response.getJSONArray(ITEMS_KEY));
-                            items.addAll(newBooks);
-                            startIndex = start + newBooks.size() + 2;
-
-                            onAllItemsFetched(items);
-                        } catch (JSONException e) {
-                            Log.e(TAG, e.getMessage(), e);
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.toString(), error);
-                onAllItemsFetched(items);
-            }
-        });
-
-        queue.add(jsonObjectRequest);
+    public void fetchMoreItems(String query){
+        Log.i(TAG, "fetchMoreItems()");
+        fetchItems(skip, startIndex, query);
     }
 
     public void onPostListing(String title, String description, String price, String course, File photoFile){
@@ -172,15 +139,5 @@ public abstract class ItemRepository {
         parseClient.saveImageToParse(photoFile);
     }
 
-    public int getCurrentSkip(){
-        return skip;
-    }
-
-    public long getCurrentStart(){
-        return startIndex;
-    }
-
-    public abstract void onAllItemsFetched(List<Item> items);
-
-    public abstract void listingSaved(Listing listing);
+    public void onAllItemsFetched(List<Item> items){}
 }
