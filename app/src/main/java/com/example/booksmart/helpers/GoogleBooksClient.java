a4dc61b96ba3e69dcca45af0a7acc2d7ce785420
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-29 22:37:31 -0400

    Created GoogleBooksClient to handle google books api fetch request

---
+++ app/src/main/java/com/example/booksmart/helpers/GoogleBooksClient.java
@@ -0,0 +1,69 @@
+package com.example.booksmart.helpers;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.booksmart.models.Book;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoogleBooksClient {
+
+    public static final String TAG = "GoogleBooksClient";
+    public static final int LISTING_LIMIT = 30;
+    public static final String GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?fields=items(id,volumeInfo,saleInfo)&printType=books&maxResults=" + String.valueOf(LISTING_LIMIT) + "&q=";
+    public static final String DEFAULT_QUERY = "college+textbook";
+    public static final String ITEMS_KEY = "items";
+
+    RequestQueue queue;
+    long startIndex;
+
+    public GoogleBooksClient(Context context){
+        queue = Volley.newRequestQueue(context);
+        startIndex = 0;
+    }
+
+    public void fetchBooks(String queryString, long start){
+        Log.i(TAG, "fetchBooks()");
+        if (queryString.equals("")){
+            queryString = DEFAULT_QUERY;
+        }
+        Log.i(TAG, queryString);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, GOOGLE_BOOKS_URL + queryString + "&startIndex=" + String.valueOf(start), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            List<Book> newBooks = Book.fromJsonArray(response.getJSONArray(ITEMS_KEY));
+                            startIndex = start + newBooks.size() + 2;
+
+                            //Log.i(TAG, newBooks.get(0).getTitle());
+                            onBooksFetched(newBooks);
+                        } catch (JSONException e) {
+                            Log.e(TAG, e.getMessage(), e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.toString(), error);
+                onBooksFetched(new ArrayList<>());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    public void onBooksFetched(List<Book> newBooks) {}
+}
