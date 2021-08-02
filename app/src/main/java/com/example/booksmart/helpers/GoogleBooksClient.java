c3d6594ecc1d35196bc18fe1acfb4dfb92d231fc
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-08-02 15:39:35 -0400

    Implemented method to fetch specific book by id from google books api

--- app/src/main/java/com/example/booksmart/helpers/GoogleBooksClient.java
+++ app/src/main/java/com/example/booksmart/helpers/GoogleBooksClient.java
@@ -23,6 +23,7 @@ public class GoogleBooksClient {
     public static final String TAG = "GoogleBooksClient";
     public static final int LISTING_LIMIT = 15;
     public static final String GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?fields=items(id,volumeInfo,saleInfo)&printType=books&maxResults=" + String.valueOf(LISTING_LIMIT) + "&q=";
+    public static final String GOOGLE_BOOKS_URL_SPECIFIC = "https://www.googleapis.com/books/v1/volumes/";
     public static final String DEFAULT_QUERY = "college+textbook";
     public static final String ITEMS_KEY = "items";
 
@@ -64,6 +65,31 @@ public class GoogleBooksClient {
 
         queue.add(jsonObjectRequest);
     }
+
+    public void fetchBook(String bookId){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, GOOGLE_BOOKS_URL_SPECIFIC + bookId, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Book book = Book.fromJson(response);
+                            onBookFetched(book);
+                        } catch (JSONException e) {
+                            Log.e(TAG, e.getMessage(), e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.toString(), error);
+                onBooksFetched(null);
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    public void onBookFetched(Book book) {}
 
     public void onBooksFetched(List<Book> newBooks) {}
 }
